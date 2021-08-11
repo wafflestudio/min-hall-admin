32e8d496d267a45ec5f2d3ff10bd0053e16effad
TaewoongMoon <taewoongmoon1@gmail.com>
2021-08-12 02:18:01 +0900

    Add: Context API for global variables usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,41 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Head from 'next/head'
+import { createContext, useState } from 'react'
+export const GlobalContext = createContext({
+  userDetail: false,
+  setUserDetail: (_: any) => {},
+  onClickUserDetailOpen: (_: any) => {},
+  onClickUserDetailClose: (_: any) => {},
+})
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [userDetail, setUserDetail] = useState<boolean>(false)
+  const onClickUserDetailOpen = () => {
+    setUserDetail(true)
+  }
+  const onClickUserDetailClose = () => {
+    setUserDetail(false)
+  }
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <GlobalContext.Provider
+        value={{
+          userDetail,
+          setUserDetail,
+          onClickUserDetailClose,
+          onClickUserDetailOpen,
+        }}
+      >
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
+  )
 }
 export default MyApp
